01cc69bad168d74eb82039b0c2ebb7e3699ca561
Joshua Radin <jradin16@gmail.com>
2021-08-22 15:15:00 -0700

    Changing generate_node to use pointers

--- src/passes/analysis/dependency_tool.rs
+++ src/passes/analysis/dependency_tool.rs
@@ -1,2 +1,19 @@
+use crate::core::identifier::Identifier;
+use std::iter::FromIterator;
+
 /// Creates dependency tags
-pub struct DependencyTool;
+pub struct DependencyTool {
+    major_id: Vec<String>
+}
+
+impl DependencyTool {
+
+    fn major_namespace(&self) -> Identifier {
+        Identifier::from_iter(&self.major_id)
+    }
+
+    fn id_within_major_namespace(&self, id: &Identifier) -> bool {
+        unimplemented!()
+    }
+}
+
