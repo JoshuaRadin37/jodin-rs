e37e6d615fee59b8bb010839aaf33cd044439b80
Joshua Radin <jradin16@gmail.com>
2021-09-07 08:11:46 -0700

    Removed pest code

--- src/parsing/mod.rs
+++ src/parsing/mod.rs
@@ -1,14 +1,7 @@
 //! Different modules that make up the parsing mechanisms for jodin. The parsing system is based
 //! off of the pest crate.
 
-#![allow(missing_docs)]
 
-#[cfg(feature = "pest_parser")]
-use pest::iterators::Pairs;
-#[cfg(feature = "pest_parser")]
-use pest::Parser;
-#[cfg(feature = "pest_parser")]
-use pest_derive::Parser;
 
 use crate::ast::{JodinNode, JodinNodeType};
 use crate::core::error::{JodinError, JodinErrorType, JodinResult};
@@ -20,171 +13,11 @@ use logos::{Lexer, Logos, Skip, SpannedIter};
 use regex::Regex;
 use std::str::{CharIndices, FromStr};
 
-/// The JodinParser. Used the pest engine to perform parsing.
-#[cfg(all(feature = "pest_parser", not(feature = "larlpop_parser")))]
-#[derive(Parser, Debug)]
-#[grammar = "parsing/jodin_grammar.pest"]
-pub struct JodinParser;
-
-/// Type alias to allow for easier refactoring
-#[cfg(feature = "pest_parser")]
-pub type JodinRule = Rule;
-
-/// Attempts to parse an input string using a JodinRule.
-///
-/// Returns an error if it fails.
-#[cfg(feature = "pest_parser")]
-pub fn complete_parse(rule: Rule, input: &str) -> JodinResult<Pairs<Rule>> {
-    let result: Result<_, pest::error::Error<_>> = JodinParser::parse(rule, input);
-    result.map_err(|err| JodinErrorType::ParserError(err, None).into())
-    //Ok(result)
-}
-
-#[cfg(test)]
-#[cfg(feature = "pest_parser")]
-mod tests {
-    use pest::Parser;
-
-    use crate::parsing::{complete_parse, JodinParser};
-
-    use super::*;
-
-    #[test]
-    fn parse_identifier() {
-        JodinParser::parse(Rule::identifier, "hello::what").unwrap();
-        JodinParser::parse(Rule::identifier, "h1::what").unwrap();
-        complete_parse(Rule::identifier, "h1::if").unwrap_err();
-        JodinParser::parse(Rule::identifier, "3as").unwrap_err();
-    }
-
-    #[test]
-    fn parse_string() {
-        assert_eq!(
-            JodinParser::parse(Rule::string, "\"hello \\\"world\"")
-                .unwrap()
-                .as_str(),
-            "\"hello \\\"world\"",
-            "Couldn't parse escapable strings"
-        );
-        assert_eq!(
-            JodinParser::parse(Rule::string, "(*\"hello \"world\"*)")
-                .unwrap()
-                .as_str(),
-            "(*\"hello \"world\"*)",
-            "Couldn't parse exact strings"
-        );
-    }
-
-    #[test]
-    fn parse_math_expression() {
-        JodinParser::parse(Rule::identifier, "true").unwrap_err();
-        JodinParser::parse(Rule::expression, "3").unwrap();
-        JodinParser::parse(Rule::expression, "3+4").unwrap();
-        JodinParser::parse(Rule::expression, "3+-4").unwrap();
-        println!(
-            "{:#?}",
-            JodinParser::parse(Rule::expression, "true || false").unwrap()
-        );
-        assert_eq!(
-            JodinParser::parse(Rule::expression, "-3+-4*5")
-                .unwrap()
-                .as_str(),
-            "-3+-4*5"
-        );
-        assert_eq!(
-            JodinParser::parse(Rule::expression, "variable.field->method()")
-                .unwrap()
-                .as_str(),
-            "variable.field->method()",
-            "Couldn't parse members"
-        );
-        assert_eq!(
-            JodinParser::parse(
-                Rule::expression,
-                "variable.method(var1->field1, var2->field() + 2)*4"
-            )
-            .unwrap()
-            .as_str(),
-            "variable.method(var1->field1, var2->field() + 2)*4",
-            "Couldn't parse members and args list"
-        );
-        complete_parse(Rule::expression, "3ul >> 2").unwrap();
-        complete_parse(Rule::expression, "true || false").unwrap();
-    }
-
-    #[test]
-    fn parse_statements() {
-        complete_parse(
-            Rule::selection_statement,
-            "\
-        if (true || false) {\
-            \
-        } else if (false) { } else;\
-        ",
-        )
-        .unwrap();
-    }
-
-    #[test]
-    fn parse_declaration() {
-        let p = complete_parse(Rule::declaration, "unsigned int red;");
-        match p.map_err(|e| e.into_err_and_bt().0) {
-            Ok(o) => println!("{:#?}", o),
-            Err(JodinErrorType::ParserError(e, None)) => {
-                println!("{}", e);
-                panic!()
-            }
-            _ => {
-                panic!()
-            }
-        }
-        let p = complete_parse(Rule::declaration, "unsigned int red(int n);");
-        match p.map_err(|e| e.into_err_and_bt().0) {
-            Ok(o) => println!("{:#?}", o),
-            Err(JodinErrorType::ParserError(e, None)) => {
-                println!("{}", e);
-                panic!()
-            }
-            _ => {
-                panic!()
-            }
-        }
-    }
-
-    #[test]
-    fn parse_function_definition() {
-        let string = "unsigned int factorial(unsigned int n) {\
-        if (n == 0) return 0;\
-        return factorial(n- 1) * n; }";
-        let p = complete_parse(Rule::function_definition, string);
-        match p.map_err(|e| e.into_err_and_bt().0) {
-            Ok(o) => println!("{:#?}", o),
-            Err(JodinErrorType::ParserError(e, None)) => {
-                println!("{:#?}", e);
-                println!("{}", e);
-                panic!()
-            }
-            _ => {
-                panic!()
-            }
-        }
-    }
-
-    #[test]
-    fn parse_structure_definition() {
-        let string = "struct basic { int* hello; };";
-        complete_parse(Rule::struct_definition, string).unwrap();
-        let string =
-            "struct basic<T <- Object> { T* hello; }; struct basic<T <- Object> { T* hello; };";
-        complete_parse(Rule::top_level_declarations, string).unwrap();
-    }
-}
-
 
 // pub mod jodin_grammar;
-#[cfg(all(not(feature = "pest_parser"), feature = "larlpop_parser"))]
-lalrpop_mod!(pub jodin_grammar, "/parsing/jodin_grammar.rs");
+lalrpop_mod!(#[allow(missing_docs)] pub jodin_grammar, "/parsing/jodin_grammar.rs");
 
+/// Shorthand for the result given by the Lexer
 pub type Spanned<Tok, Loc> = Result<(Loc, Tok, Loc), JodinError>;
 
 /// The jodin lexer
@@ -223,6 +56,7 @@ impl<'input> Iterator for JodinLexer<'input> {
     }
 }
 
+#[allow(missing_docs)]
 #[derive(Debug, Copy, Clone, Logos)]
 pub enum Tok<'input> {
     #[regex(r"[ \t\n\f]+", logos::skip)]
@@ -386,11 +220,15 @@ pub enum Tok<'input> {
     Error,
 }
 
+/// A part of an expression
 pub enum ExpressionMember {
+    /// An indivisible part of the expression
     Factor(JodinNode),
+    /// An operator in the expression
     Op(Operator),
 }
 
+/// Turns a list of expression members into an AST tree
 pub fn into_order_of_operations(segments: Vec<ExpressionMember>) -> JodinNode {
     let mut expression_stack = vec![];
     let mut op_stack: Vec<Operator> = vec![];
