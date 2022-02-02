83323a12b7240dc14f3a8c1d5aa4e7af0fae925e
Joshua Radin <jradin16@gmail.com>
2022-02-02 01:15:57 -0800

    #78: added base plugin

---
+++ jodin-gradle-plugin/src/main/groovy/joshradin/jodin/JodinBase.groovy
@@ -0,0 +1,13 @@
+package joshradin.jodin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+@SuppressWarnings("unused")
+class JodinBase implements Plugin<Project>{
+    @Override
+    void apply(Project project) {
+
+
+    }
+}
